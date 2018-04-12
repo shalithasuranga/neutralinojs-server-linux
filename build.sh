7606d1d5c2c675d8a95ee82f16f0e6d8d2d8d0d7
Shalitha <shalithasuranga@gmail.com>
2018-04-12 22:47:11 +0530

    Change into c++ from c

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ if [ -e bin/neutranino.out ]; then
     rm bin/neutralino.out
 fi
 
-gcc src/httpserver.c -pthread -o bin/neutralino -w
+g++ -std=c++11  src/Buffer.cpp src/EventLoop.cpp src/EventLoopThreadPool.cpp src/Parser.cpp src/Socket.cpp src/Epoll.cpp src/EventLoopThread.cpp src/Handler.cpp src/main.cpp src/Thread.cpp -pthread -o bin/neutralino -w
---
+++ src/Parser.h
@@ -0,0 +1,40 @@
+/*
+ * Author: Broglie 
+ * E-mail: yibo141@outlook.com
+ */
+
+#ifndef PARSER_H
+#define PARSER_H
+
+#include <vector>
+#include <string>
+#include <cassert>
+#include <iostream>
+#include <algorithm>
+#include <string.h>
+
+// 解析请求后的数据存储在http_request结构体中
+typedef struct
+{
+    std::string method;     // 请求的方法
+    std::string uri;        // 请求的uri
+    std::string version;    // HTTP版本
+    std::string host;       // 请求的主机名
+    std::string connection; // Connection首部
+} HTTPRequest;
+
+class Parser
+{
+public:
+    Parser(const std::string request);
+    HTTPRequest getParseResult();
+private:
+    void parseLine();        // 将请求按行解析存入_lines数组中
+    void parseRequestLine(); // 解析请求行
+    void parseHeaders();     // 解析头部字段
+    std::string _request;    // 客户的原始请求
+    std::vector<std::string> _lines;
+    HTTPRequest _parseResult;
+};
+
+#endif // PARSER_H
