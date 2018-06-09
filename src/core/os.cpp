1d13c8ae0264afd2e3af3eb053c397f42e39e277
Shalitha <shalithasuranga@gmail.com>
2018-06-09 21:06:05 +0530

    Implement runCommand

--- src/core/os.cpp
+++ src/core/os.cpp
@@ -17,7 +17,27 @@ namespace os {
     string runCommand(string jso) {
         json input;
         json output;
-
+        try {
+            input = json::parse(jso);
+        }
+        catch(exception e){
+            output["error"] = "JSON parse error is occurred!";
+            return output.dump();
+        }
+        string command = input["command"];
+        std::array<char, 128> buffer;
+        std::string result;
+        std::shared_ptr<FILE> pipe(popen(command.c_str(), "r"), pclose);
+        if (!pipe) {
+            output["error"] = "Pipe open failed";
+        }
+        else {
+            while (!feof(pipe.get())) {
+                if (fgets(buffer.data(), 128, pipe.get()) != nullptr)
+                    result += buffer.data();
+            }
+            output["stdout"] = result;
+        }
         return output.dump();
        
         
